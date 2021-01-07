80b114e1748f34963ed39a002565a9888666ce49
bgdtomas <mtmbogado21@gmail.com>
2021-01-06 21:51:23 -0300

    Slideshow ended, still missing mobile

--- perla-zone.js
+++ perla-zone.js
@@ -1,25 +1,24 @@
 function myFunction(button) {
-    var x = document.getElementById("myTopnav");
-    button.classList.toggle("change")
-    if (x.className === "topnav") {
-      x.className += " responsive";
-      document.body.style.overflow = "hidden";
-      document.getElementById("MySections").style.display = "none";
-    } else {
-      x.className = "topnav";
-      document.body.style.overflow = "scroll";
-      document.getElementById("MySections").style.display = "block";
-    }
+  var x = document.getElementById("myTopnav");
+  button.classList.toggle("change");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+    document.body.style.overflow = "hidden";
+    document.getElementById("MySections").style.display = "none";
+  } else {
+    x.className = "topnav";
+    document.body.style.overflow = "scroll";
+    document.getElementById("MySections").style.display = "block";
   }
-  
-  //Smooth scrolling with links
+}
 
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+//Smooth scrolling with links
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
     });
+  });
 });
