1a8d50695a647f5cf473ecca2f1f93e07e6dcb51
bgdtomas <mtmbogado21@gmail.com>
2021-02-10 10:52:25 -0300

    changed faq to an accordion section

--- slideshow-script.js
+++ slideshow-script.js
@@ -1,42 +1,55 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+// var slideIndex = 1;
+// showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides((slideIndex += n));
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides((slideIndex += n));
+// }
+
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides((slideIndex = n));
+// }
+
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("faq");
+//   if (n > slides.length) {
+//     slideIndex = 1;
+//   }
+//   if (n < 1) {
+//     slideIndex = slides.length;
+//   }
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slides[slideIndex - 1].style.display = "block";
+// }
+
+var acc = document.getElementsByClassName("collapsibleButton");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("faq");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex - 1].style.display = "block";
-}
-
-
-//----------------------------------------------------------------
+// //----------------------------------------------------------------
 
 var slideIndexGallery = 1;
-showSlidesGallery(slideIndexGallery)
+showSlidesGallery(slideIndexGallery);
 
 // Next/previous controls
 function plusSlidesGallery(n) {
-  if (screen.width <= 600){
+  if (screen.width <= 600) {
     showSlidesGallery((slideIndexGallery += n));
-
   }
 }
 
