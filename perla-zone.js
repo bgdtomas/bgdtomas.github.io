a22b5b65045da8d7d804d120e3583739484e9826
bgdtomas <mtmbogado21@gmail.com>
2021-02-11 14:32:19 -0300

    nav menu works on mobile res

--- perla-zone.js
+++ perla-zone.js
@@ -3,14 +3,10 @@ function myFunction(button) {
   button.classList.toggle("change");
   if (x.className === "topnav") {
     x.className += " responsive";
-    document.body.style.overflow = "hidden";
-    document.getElementById("mySections").style.display = "none";
-    document.getElementById("footer").style.display = "none";
+    document.getElementById("navList").style.display = "block";
   } else {
     x.className = "topnav";
-    document.body.style.overflow = "scroll";
-    document.getElementById("mySections").style.display = "block";
-    document.getElementById("footer").style.display = "block";
+    document.getElementById("navList").style.display = "none";
   }
 }
 
