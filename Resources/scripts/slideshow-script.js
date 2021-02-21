1e687c266fd5af16c09e891e4af0bace94e3055a
bgdtomas <mtmbogado21@gmail.com>
2021-02-21 13:44:25 -0300

    changes to the files pathing

--- Resources/scripts/slideshow-script.js
+++ Resources/scripts/slideshow-script.js
@@ -1,30 +1,3 @@
-// var slideIndex = 1;
-// showSlides(slideIndex);
-
-// // Next/previous controls
-// function plusSlides(n) {
-//   showSlides((slideIndex += n));
-// }
-
-// // Thumbnail image controls
-// function currentSlide(n) {
-//   showSlides((slideIndex = n));
-// }
-
-// function showSlides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName("faq");
-//   if (n > slides.length) {
-//     slideIndex = 1;
-//   }
-//   if (n < 1) {
-//     slideIndex = slides.length;
-//   }
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   slides[slideIndex - 1].style.display = "block";
-// }
 
 var acc = document.getElementsByClassName("collapsibleButton");
 var i;
