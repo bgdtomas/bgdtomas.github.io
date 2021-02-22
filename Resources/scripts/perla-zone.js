db9a6d0b94515381eaa628cf3b9b2e08bbb813a6
bgdtomas <mtmbogado21@gmail.com>
2021-02-22 10:34:47 -0300

    tweaks

--- Resources/scripts/perla-zone.js
+++ Resources/scripts/perla-zone.js
@@ -1,33 +1,30 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-  function myFunction(button) {
-    var x = document.getElementById("myTopnav");
-    var b = document.getElementById("button");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-      b.className += " change";
-      document.body.style.overflow = "scroll";
-      document.getElementById("mySections").style.display = "none";
-      document.getElementById("footer").style.display = "none";
-      document.getElementById("navList").style.display = "block";
-    } else {
-      x.className = "topnav";
-      b.classList.remove("change");
-      document.body.style.overflow = "scroll";
-      document.getElementById("mySections").style.display = "block";
-      document.getElementById("footer").style.display = "grid";
-      document.getElementById("navList").style.display = "none";
-    }
+function myFunction(button) {
+  var x = document.getElementById("myTopnav");
+  var b = document.getElementById("button");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+    b.className += " change";
+    document.body.style.overflow = "scroll";
+    document.getElementById("mySections").style.display = "none";
+    document.getElementById("footer").style.display = "none";
+    document.getElementById("navList").style.display = "block";
+  } else {
+    x.className = "topnav";
+    b.classList.remove("change");
+    document.body.style.overflow = "scroll";
+    document.getElementById("mySections").style.display = "block";
+    document.getElementById("footer").style.display = "grid";
+    document.getElementById("navList").style.display = "none";
   }
-  
-  //Smooth scrolling with links
-  
-  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-    anchor.addEventListener("click", function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute("href")).scrollIntoView({
-        behavior: "smooth",
-      });
+}
+
+//Smooth scrolling with links
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
     });
   });
-  
 });
